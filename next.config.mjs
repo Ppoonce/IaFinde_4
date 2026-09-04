7252b938c498bd45559420bbea639926405a94a3
Emmanuel <124106502+Ppoonce@users.noreply.github.com>
2026-09-04 10:42:18 -0400

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,1 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  images: { unoptimized: true },
-  trailingSlash: false,
-};
-
-export default nextConfig;
+ /** @type {import('next').NextConfig} */ const isGithubPages = process.env.GITHUB_PAGES === 'true'; const repoName = '/IaFinde_4'; const nextConfig = { output: 'export', images: { unoptimized: true }, trailingSlash: true, basePath: isGithubPages ? repoName : '', assetPrefix: isGithubPages ? repoName : '', }; export default nextConfig;
