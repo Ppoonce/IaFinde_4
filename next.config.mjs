0692772984f80b7f53662a2ff0f729167b67c8cc
Emmanuel <124106502+Ppoonce@users.noreply.github.com>
2026-09-04 23:34:15 -0400

    Delete next.config.mjs

--- next.config.mjs
+++
@@ -1,1 +0,0 @@
- /** @type {import('next').NextConfig} */ const isGithubPages = process.env.GITHUB_PAGES === 'true'; const repoName = '/IaFinde_4'; const nextConfig = { output: 'export', images: { unoptimized: true }, trailingSlash: true, basePath: isGithubPages ? repoName : '', assetPrefix: isGithubPages ? repoName : '', }; export default nextConfig;
